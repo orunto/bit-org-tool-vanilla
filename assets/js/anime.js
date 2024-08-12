b2e6c180be0e9d554e7282a33834d40a1cfe1c42
orunto <oruntoeniola@aol.com>
2024-08-12 12:23:03 +0100

    feat: menu

---
+++ assets/js/anime.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+  function show() {
+    let all = document.querySelectorAll("aside button span");
+    for (let i = 0; i < all.length; i++) {
+      all[i].style.display = "flex";
+    }
+
+
+    document.getElementById("menu").removeEventListener("click", show);
+    document.getElementById("menu").addEventListener("click", hide);
+  }
+
+  function hide() {
+    let all = document.querySelectorAll("aside button span");
+    for (let i = 0; i < all.length; i++) {
+      all[i].style.display = "none";
+    }
+    document.getElementById("menu").removeEventListener("click", hide);
+    document.getElementById("menu").addEventListener("click", show);
+  }
+
+  document.getElementById("menu").addEventListener("click", show);
+};
