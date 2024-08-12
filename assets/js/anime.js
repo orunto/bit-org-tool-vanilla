89872826ee1a3c720819c240bcade573849d11bd
orunto <oruntoeniola@aol.com>
2024-08-12 22:27:21 +0100

    feat: dropdowns

--- assets/js/anime.js
+++ assets/js/anime.js
@@ -27,7 +27,31 @@ window.onload = function () {
     document.getElementById("reasons").style.translate = "0px -1000px"
   }
 
+  function notify () {
+    document.getElementById("notifs").style.display = "flex"
+    document.getElementById("bell").removeEventListener("click", notify);
+    document.getElementById("bell").addEventListener("click", closenotify);
+  }
+
+  function closenotify () {
+    document.getElementById("notifs").style.display = "none"
+    document.getElementById("bell").removeEventListener("click", closenotify);
+    document.getElementById("bell").addEventListener("click", notify);
+  }
+
+  function logout () {
+    document.getElementById("logout").style.display = "flex"
+    document.getElementById("dropdown").removeEventListener("click", logout);
+    document.getElementById("dropdown").addEventListener("click", closelogout);
+  }
+
+  function closelogout () {
+    document.getElementById("logout").style.display = "none"
+    document.getElementById("dropdown").removeEventListener("click", closelogout);
+    document.getElementById("dropdown").addEventListener("click", logout);
+  }
+
   document.getElementById("menu").addEventListener("click", show);
-  document.getElementById("test").addEventListener("click", reason);
-  document.getElementById("cancel").addEventListener("click", retract);
+  document.getElementById("bell").addEventListener("click", notify);
+  document.getElementById("dropdown").addEventListener("click", logout);
 };
